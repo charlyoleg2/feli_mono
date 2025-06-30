0ac67f8c137fcbd33b64157c828bd5608cc8c633
charlyoleg <charles.braquet@gmx.fr>
2025-06-30 22:08:19 +0200

    import path

--- pkg/check-feli/src/check-feli.ts
+++ pkg/check-feli/src/check-feli.ts
@@ -3,6 +3,7 @@
 
 import { feli_cli } from 'feli';
 import process from 'node:process';
+import path from 'node:path':
 
 const scrDir = import.meta.dirname;
 const defaultPublicDir = path.join(scrDir, 'public');
