860abf0dff20b4a9ac9705a7918872431c2a2880
charlyoleg <charles.braquet@gmx.fr>
2025-06-30 19:08:34 +0200

    fir format

--- pkg/feli/src/index.test.ts
+++ pkg/feli/src/index.test.ts
@@ -7,6 +7,8 @@ describe('feli suit', () => {
 		await expect(feli_cli('pubDir', ['node-path', 'script-path'])).rejects.toThrowError();
 	});
 	it('feli with existing path', async () => {
-		expect(await feli_cli('scr/public', ['node-path', 'script-path', '--browser=0'])).toBeUndefined();
+		expect(
+			await feli_cli('scr/public', ['node-path', 'script-path', '--browser=0'])
+		).toBeUndefined();
 	});
 });
