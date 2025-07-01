f2c2427e4ba313c10973d819699882e1359323ec
charlyoleg <charles.braquet@gmx.fr>
2025-07-01 16:15:57 +0200

    layout

---
+++ pkg/feli-pages/src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import type { Snippet } from 'svelte';
+
+	// props
+	interface Props {
+		children?: Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<header>
+	<a href={base}>F</a>
+	<h1>Feli pages</h1>
+</header>
+
+<main>
+	{@render children?.()}
+</main>
+
+<footer>
+	<h3>Links around Feli</h3>
+	<ol>
+		<li><a href="https://www.npmjs.com/package/feli">npm package</a></li>
+		<li><a href="https://www.npmjs.com/package/create-feli">npm package create</a></li>
+		<li><a href="https://github.com/charlyoleg2/feli_mon">Github</a></li>
+		<li><a href="https://charlyoleg2.github.io/feli_mon">Github pages</a></li>
+	</ol>
+</footer>
+
+<style>
+	:global(body) {
+		font-family: 'Courier New', 'Helvetica', 'Arial', 'Verdana';
+		margin: 0;
+		background-color: mediumAquamarine;
+	}
+	main {
+		background-color: lightCyan;
+		min-height: 70vh;
+	}
+</style>
--- pkg/feli-pages/svelte.config.js
+++ pkg/feli-pages/svelte.config.js
@@ -2,6 +2,9 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const basePath = process.env.BASE_PATH;
+const dev = process.argv.includes('dev') || basePath === undefined;
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx', '.md'],
@@ -13,7 +16,13 @@ const config = {
 			extensions: ['.svx', '.md']
 		})
 	],
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : `/${basePath}`,
+			relative: true // true and false works!!!
+		}
+	}
 };
 
 export default config;
