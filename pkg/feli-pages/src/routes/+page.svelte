3bdefdde6b41c740a01677649ea9c60fea6d906a
charlyoleg <charles.braquet@gmx.fr>
2025-12-27 01:25:23 +0100

    update svelte

--- pkg/feli-pages/src/routes/+page.svelte
+++ pkg/feli-pages/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { base } from '$app/paths';
+	import { resolve, asset } from '$app/paths';
 </script>
 
 <h1>Feli, the Front-End Locally Installed</h1>
 
 <h2>Docs of Feli</h2>
 <ol>
-	<li><a href="{base}/docs/apidoc/" rel="external">Feli API</a></li>
-	<li><a href="{base}/docs/concepts/">Feli concepts</a></li>
-	<li><a href="{base}/docs/readme/">readme</a></li>
+	<li><a href={asset('/docs/apidoc/')} rel="external">Feli API</a></li>
+	<li><a href={resolve('/docs/concepts/')}>Feli concepts</a></li>
+	<li><a href={resolve('/docs/readme/')}>readme</a></li>
 </ol>
